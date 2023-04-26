a89ae156a856ba634b403b5501f005404737be13
Drachurf <121042040+Drachurf@users.noreply.github.com>
2023-04-26 20:23:10 +0200

    controlleurATester

--- backend/controleurs/book.js
+++ backend/controleurs/book.js
@@ -1,8 +1,7 @@
 const Book = require("../models/book.js");
 const fs = require("fs");
 const sharp = require("sharp");
-const path = require('path')
-
+const path = require("path");
 
 exports.createBook = (req, res, next) => {
   // Vérifie que req.file existe.
@@ -10,7 +9,7 @@ exports.createBook = (req, res, next) => {
     return res.status(400).json({ error: "Veuillez sélectionner une image." });
   }
   const bookObject = JSON.parse(req.body.book);
-  
+
   // Ajoute l'identifiant utilisateur actuel aux propriétés de bookObject.
   bookObject._userId = req.userId;
 
@@ -20,59 +19,63 @@ exports.createBook = (req, res, next) => {
   // Crée une nouvelle instance du modèle Book avec les propriétés de bookObject et l'URL de l'image ajoutée à la requête.
   const book = new Book({
     ...bookObject,
-    imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${
+      req.file.filename
+    }`,
     averageRating: 0,
-    rating: []
+    rating: [],
   });
 
   // Redimensionne l'image et l'enregistre dans le dossier d'images.
   sharp(req.file.path)
-  .webp({ quality: 80 })
-  .toBuffer((err, buffer) => {
-    if (err) {
-      return res.status(400).json({ error: err.message });
-    }
-
-    const parsedPath = path.parse(req.file.filename);
-    const newFilename = `compressed_${parsedPath.name}.webp`;
-
-    fs.writeFile(`images/${newFilename}`, buffer, (err) => {
+    .webp({ quality: 80 })
+    .toBuffer((err, buffer) => {
       if (err) {
         return res.status(400).json({ error: err.message });
       }
 
-      // Supprime l'image originale
-      fs.unlink(req.file.path, (err) => {
+      const parsedPath = path.parse(req.file.filename);
+      const newFilename = `compressed_${parsedPath.name}.webp`;
+
+      fs.writeFile(`images/${newFilename}`, buffer, (err) => {
         if (err) {
-          console.error(err);
+          return res.status(400).json({ error: err.message });
         }
-      });
 
-      // Met à jour l'URL de l'image pour pointer vers la nouvelle image compressée.
-      book.imageUrl = `${req.protocol}://${req.get("host")}/images/${newFilename}`;
-
-      // Enregistre le nouveau livre dans la base de données.
-      book
-        .save()
-        .then(() => {
-          res.status(201).json({ message: "Post saved successfully!" });
-        })
-        .catch((error) => {
-          // En cas d'erreur, supprime l'image compressée.
-          fs.unlink(`images/compressed_${req.file.filename}.webp`, (err) => {
-            if (err) {
-              console.error(err);
-            }
-          });
-          res.status(400).json({ error: error });
+        // Supprime l'image originale
+        fs.unlink(req.file.path, (err) => {
+          if (err) {
+            console.error(err);
+          }
         });
+
+        // Met à jour l'URL de l'image pour pointer vers la nouvelle image compressée.
+        book.imageUrl = `${req.protocol}://${req.get(
+          "host"
+        )}/images/${newFilename}`;
+
+        // Enregistre le nouveau livre dans la base de données.
+        book
+          .save()
+          .then(() => {
+            res.status(201).json({ message: "Post saved successfully!" });
+          })
+          .catch((error) => {
+            // En cas d'erreur, supprime l'image compressée.
+            fs.unlink(`images/compressed_${req.file.filename}.webp`, (err) => {
+              if (err) {
+                console.error(err);
+              }
+            });
+            res.status(400).json({ error: error });
+          });
+      });
     });
-  });
 };
 
 exports.modifyBook = (req, res, next) => {
   // Vérifie si une image est attachée à la requête POST, et modifie les propriétés de l'objet bookObject en conséquence.
-  const bookObject = req.file
+  let bookObject = req.file
     ? {
         ...JSON.parse(req.body.book),
         imageUrl: `${req.protocol}://${req.get("host")}/images/${
@@ -83,68 +86,84 @@ exports.modifyBook = (req, res, next) => {
   // Supprime la propriété _userId de l'objet bookObject.
   delete bookObject._userId;
 
+  console.log("bookObject:", bookObject);
+
   // Vérifie si l'utilisateur est autorisé à modifier ce livre.
-  Book.findOne({ _id: req.params.id })
-    .then((book) => {
-      if (!book) {
-        return res.status(404).json({ error: "Book not found" });
-      }
+  Book.findOne({ _id: req.params.id }).then((book) => {
+    console.log("book:", book);
+    // Si l'utilisateur n'est pas autorisé à modifier ce livre : erreur 403
+    if (book.userId != req.userId) {
+      res.status(403).json({ message: "Action non autorisée" });
+    }
+    // Supprime l'ancienne image si elle existe et si la nouvelle image est différente de l'ancienne
+    else if (
+      book.imageUrl &&
+      req.file &&
+      req.file.filename !== book.imageUrl.split("/").pop()
+    ) {
+      const oldImagePath = book.imageUrl.split("/images/")[1];
+      fs.unlinkSync(`images/${oldImagePath}`);
+    }
+    // Redimensionne l'image et l'enregistre dans le dossier d'images si une image est attachée à la requête POST.
+    if (req.file) {
+      sharp(req.file.path)
+        .webp({ quality: 80 })
+        .toBuffer((err, buffer) => {
+          if (err) {
+            return res.status(400).json({ error: err.message });
+          }
 
-      if (book.userId !== req.auth.userId) {
-        return res.status(403).json({ error: "Unauthorized request" });
-      }
+          const parsedPath = path.parse(req.file.filename);
+          const newFilename = `compressed_${parsedPath.name}.webp`;
 
-      // Supprime l'ancienne image si elle existe et si la nouvelle image est différente de l'ancienne
-      if (
-        book.imageUrl &&
-        req.file &&
-        req.file.filename !== book.imageUrl.split("/").pop()
-      ) {
-        const oldImagePath = book.imageUrl.split("/images/")[1];
-        fs.unlinkSync(`images/${oldImagePath}`);
-      }
-      // Redimensionne l'image et l'enregistre dans le dossier d'images.
-      if (req.file) {
-        sharp(req.file.path)
-          .webp({ quality: 80 })
-          .toBuffer((err, buffer) => {
+          fs.writeFile(`images/${newFilename}`, buffer, (err) => {
             if (err) {
               return res.status(400).json({ error: err.message });
             }
-
-            const parsedPath = path.parse(req.file.filename);
-            const newFilename = `compressed_${parsedPath.name}.webp`;
-
-            fs.writeFile(`images/${newFilename}`, buffer, (err) => {
-              if (err) {
-                return res.status(400).json({ error: err.message });
-              }
-              // Une fois l'image redimensionnée et enregistrée, supprime l'image originale si la nouvelle image est différente de l'ancienne.
-              if (req.file.filename !== book.imageUrl.split("/").pop()) {
-                fs.unlink(req.file.path, (err) => {
-                  if (err) {
-                    return res.status(400).json({ error: err.message });
-                  }
-                });
-              }
-              // Met à jour l'URL de l'image pour pointer vers la nouvelle image redimensionnée.
-              bookObject.imageUrl = `${req.protocol}://${req.get(
-                "host"
-              )}/images/${newFilename}`;
-              // Met à jour le livre dans la base de données avec les propriétés modifiées de bookObject.
-              Book.updateOne(
-                { _id: req.params.id },
-                { ...bookObject, _id: req.params.id }
-              )
-                .then(() => res.status(200).json({ message: "Objet modifié!" }))
-                .catch((error) => res.status(401).json({ error }));
+            // Une fois l'image redimensionnée et enregistrée, supprime l'image originale si la nouvelle image est différente de l'ancienne.
+            if (req.file.filename !== book.imageUrl.split("/").pop()) {
+              fs.unlink(req.file.path, (err) => {
+                if (err) {
+                  return res.status(400).json({ error: err.message });
+                }
               });
-            });
-        }
-    })
-    .catch((error) => {
-      res.status(400).json({ error });
-    });
+            }
+            // Met à jour l'URL de l'image pour pointer vers la nouvelle image redimensionnée.
+            bookObject.imageUrl = `${req.protocol}://${req.get(
+              "host"
+            )}/images/${newFilename}`;
+            // Met à jour le livre dans la base de données avec les propriétés modifiées de bookObject.
+            console.log("updating book with:", bookObject);
+            Book.updateOne(
+              { _id: req.params.id },
+              { ...bookObject, _id: req.params.id }
+            )
+              .then(() => {
+                console.log("book updated");
+                res.status(200).json({ message: "Objet modifié!" });
+              })
+              .catch((error) => {
+                console.error("error while updating book:", error);
+                res.status(401).json({ error });
+              });
+          });
+        });
+    } else {
+      // Si aucune image n'est attachée à la requête POST, met simplement à jour le livre dans la base de données avec les propriétés modifiées de bookObject.
+      Book.updateOne(
+        { _id: req.params.id },
+        { ...bookObject, _id: req.params.id }
+      )
+        .then(() => {
+          console.log("book updated");
+          res.status(200).json({ message: "Objet modifié!" });
+        })
+        .catch((error) => {
+          console.error("error while updating book:", error);
+          res.status(401).json({ error });
+        });
+    }
+  });
 };
 
 exports.Ratings = (req, res, next) => {
@@ -161,7 +180,9 @@ exports.Ratings = (req, res, next) => {
       }
       // Vérifier que la note est un entier compris entre 0 et 5
       if (grade < 0 || grade > 5) {
-        return res.status(400).json({ error: "La note doit être comprise entre 0 et 5." });
+        return res
+          .status(400)
+          .json({ error: "La note doit être comprise entre 0 et 5." });
       }
       const alreadyRated = book.ratings.some(
         (rating) => rating.userId === userId
